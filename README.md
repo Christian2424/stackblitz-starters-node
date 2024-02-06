85cb00ffed6d00e27ca3bdb2d56c54ad5b2e5050
Christian2424 <132666224+Christian2424@users.noreply.github.com>
2024-02-06 10:27:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-node
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Christian2424/stackblitz-starters-node)
---
+++ index.js
@@ -0,0 +1,11 @@
+function zeroToCento(){
+  for(var i = 0;i<101;i++){
+    console.log(i);
+
+  }
+}
+  zeroToCento()
+
+  console.log("La somma è "+(2+3));
+  console.log("La somma è " + (20 + (20*0.22)));
+
